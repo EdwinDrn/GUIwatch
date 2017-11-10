6d7545ce3ca72db95235af6c937d0b4eadac2233
Edwin Duran <EdwinDuran@Edwins-MacBook-Pro.local>
2017-11-09 23:30:39 -0500

    Add All Files

---
+++ tkinter_watch.py
@@ -0,0 +1,25 @@
+import datetime
+from datetime import datetime
+import Tkinter
+from Tkinter import *
+
+
+class EdwinButtons():
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.printButton = Button(frame, text = datetime.now().time(), command = self.watch)
+        self.printButton.pack(side = LEFT)
+
+        self.quitButton = Button(frame, text = 'quit', command = frame.quit) #This breaks main loop
+        self.quitButton.pack(side = LEFT)
+
+    def watch(self):
+        print datetime.now()
+
+
+root = Tk()
+edwin = EdwinButtons(root)
+root.mainloop()
